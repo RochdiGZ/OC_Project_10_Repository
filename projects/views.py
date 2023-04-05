855486f201a0037f87f60575b4f83da19b4184d2
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2023-04-05 03:38:59 +0100

    Commit 05 April 2023

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from rest_framework import viewsets, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Project, Contributor, Issue, Comment
